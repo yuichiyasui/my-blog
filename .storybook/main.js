ab2b49fda4981a3f2f4e903e0ab762ff2aa11191
yuichiyasui <yassan.b.a.w@gmail.com>
2022-02-27 23:34:50 +0900

    importのパスをaliasを使用するように修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   typescript: { reactDocgen: false },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -9,5 +11,16 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve = {
+      extensions: ['.ts', '.tsx', '.js', '.css'],
+      alias: {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      },
+    };
+
+    return config;
   },
 };
---
+++ src/components/pages/top/Top.tsx
@@ -0,0 +1,7 @@
+export const Top = () => {
+  return (
+    <>
+      <h1>トップ</h1>
+    </>
+  );
+};
