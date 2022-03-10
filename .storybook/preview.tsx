acb4224d144abe5aa6fe0a43c5d295162596d7f5
yuichiyasui <yassan.b.a.w@gmail.com>
2022-03-11 00:50:19 +0900

    HeaderにTopへのリンクを追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,16 @@ import "../src/styles/globals.css";
 import * as NextImage from "next/image";
 import { ImageProps } from "next/image";
 
+import { addDecorator } from "@storybook/react";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
+
+/** next/routerのモック化 */
+addDecorator((Story) => (
+  <MemoryRouterProvider>
+    <Story />
+  </MemoryRouterProvider>
+));
+
 /** next/imageのモック化 */
 const OriginalNextImage = NextImage.default;
 Object.defineProperty(NextImage, "default", {
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,14 +1,27 @@
 import { Logo } from "@/components/ui/Logo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Props = {
   className?: string;
 };
 
 export const Header = ({ className = "" }: Props) => {
+  const { pathname } = useRouter();
+  const isTop = pathname === "/";
+
   return (
     <header className={`bg-white shadow-sm w-full ${className}`}>
       <div className="py-[10px] mx-auto w-[92%] max-w-[1200px]">
-        <Logo />
+        {isTop ? (
+          <Logo />
+        ) : (
+          <Link href="/">
+            <a>
+              <Logo />
+            </a>
+          </Link>
+        )}
       </div>
     </header>
   );
