e0881cec03e3f9aaced7a9865cd1a168c5cdfcbc
yuichiyasui <yassan.b.a.w@gmail.com>
2022-03-14 01:15:03 +0900

    パンくずリストに構造化の属性を追加

--- src/components/ui/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -11,21 +11,35 @@ type Props = {
 
 export const Breadcrumbs = ({ breadcrumbs }: Props) => {
   return (
-    <ol className="flex">
-      {breadcrumbs.map((breadcrumb) => {
-        return (
-          <li
-            key={breadcrumb.label}
-            className="after:mx-1.5 text-xs after:text-gray-400 after:content-['/'] after:last:content-none sm:text-sm"
-          >
-            {breadcrumb.href ? (
-              <TextLink href={breadcrumb.href}>{breadcrumb.label}</TextLink>
-            ) : (
-              <span>{breadcrumb.label}</span>
-            )}
-          </li>
-        );
-      })}
-    </ol>
+    <nav>
+      <ol
+        itemScope
+        itemType="http://schema.org/BreadcrumbList"
+        className="flex"
+      >
+        {breadcrumbs.map((breadcrumb, index) => {
+          return (
+            <li
+              key={breadcrumb.label}
+              itemProp="itemListElement"
+              itemScope
+              itemType="http://schema.org/Listitem"
+              className="after:mx-1.5 text-xs after:text-gray-400 after:content-['/'] after:last:content-none sm:text-sm"
+            >
+              {breadcrumb.href ? (
+                <TextLink itemProp="name" href={breadcrumb.href}>
+                  {breadcrumb.label}
+                </TextLink>
+              ) : (
+                <span itemProp="name" className="tracking-wider">
+                  {breadcrumb.label}
+                </span>
+              )}
+              <meta itemProp="position" content={`${index + 1}`} />
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 };
--- src/components/ui/TextLink/TextLink.tsx
+++ src/components/ui/TextLink/TextLink.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { AnchorHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
 type Props = {
   href: string;
   children: ReactNode;
   className?: string;
-};
+} & DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
 
-export const TextLink = ({ href, children, className = "" }: Props) => {
+export const TextLink = ({
+  href,
+  children,
+  className = "",
+  ...props
+}: Props) => {
   return (
     <Link href={href}>
       <a
+        {...props}
         className={`tracking-wide text-sky-600 hover:text-sky-900 transition-colors duration-200 ${className}`}
       >
         {children}
