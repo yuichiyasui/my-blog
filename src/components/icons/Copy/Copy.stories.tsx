7e1a3f6d31fc5f106a5ca530de193654633fc87c
yuichiyasui <yassan.b.a.w@gmail.com>
2022-03-14 01:49:57 +0900

    アイコンコンポーネントを作成

---
+++ src/components/icons/Copy/Copy.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { CopyIcon } from "./Copy";
+
+export default {
+  title: "icons/Copy",
+  component: CopyIcon,
+} as ComponentMeta<typeof CopyIcon>;
+
+export const Default: ComponentStoryObj<typeof CopyIcon> = {};
---
+++ src/components/icons/LINE/LINE.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { LINEIcon } from "./LINE";
+
+export default {
+  title: "icons/LINE",
+  component: LINEIcon,
+} as ComponentMeta<typeof LINEIcon>;
+
+export const Default: ComponentStoryObj<typeof LINEIcon> = {};
