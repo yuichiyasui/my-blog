5fa01274a0c0c03f42691896a1e2f8cfa620e5b9
yuichiyasui <yassan.b.a.w@gmail.com>
2022-03-19 21:47:58 +0900

    aタグにTextLinkコンポーネントを対応させる

--- src/components/ui/TextLink/TextLink.tsx
+++ src/components/ui/TextLink/TextLink.tsx
@@ -1,8 +1,9 @@
+import { ExternalLink } from "@/components/functionals/ExternalLink";
 import Link from "next/link";
 import { AnchorHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
 type Props = {
-  href: string;
+  href: string | undefined;
   children: ReactNode;
   className?: string;
 } & DetailedHTMLProps<
@@ -10,18 +11,35 @@ type Props = {
   HTMLAnchorElement
 >;
 
+const baseClass =
+  "leading-relaxed tracking-wider text-sky-600 hover:text-sky-900 transition-colors duration-200";
+
 export const TextLink = ({
   href,
   children,
   className = "",
   ...props
 }: Props) => {
+  if (!href) {
+    return <span className={className}>{children}</span>;
+  }
+
+  const isUrl = /(http|https):\//.test(href);
+  if (isUrl) {
+    return (
+      <ExternalLink
+        {...props}
+        href={href}
+        className={`${baseClass} ${className}`}
+      >
+        {children}
+      </ExternalLink>
+    );
+  }
+
   return (
     <Link href={href}>
-      <a
-        {...props}
-        className={`tracking-wide text-sky-600 hover:text-sky-900 transition-colors duration-200 ${className}`}
-      >
+      <a {...props} className={`${baseClass} ${className}`}>
         {children}
       </a>
     </Link>
