46898780dd790da9839d0a82cf77811103ecde94
yuichi yasui <56911907+yuichiyasui@users.noreply.github.com>
2022-03-21 11:08:21 +0900

    デプロイ条件を修正 (#44)
    * mainブランチにpush時にVercelビルドされるように修正
    
    * デプロイ条件を修正

--- scripts/vercel-ignore-build-step.sh
+++ scripts/vercel-ignore-build-step.sh
@@ -4,7 +4,8 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 # developブランチにpushされた際のみpreview deployを行う
 # 参考: https://zenn.dev/catnose99/articles/b37104fc7ef214
-if "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main" ; then
+# https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel#with-environment-variables 
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
