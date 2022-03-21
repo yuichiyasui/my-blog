4c3f72866f2b0e69e890a9bb4fbfae20d476252c
yuichi yasui <56911907+yuichiyasui@users.noreply.github.com>
2022-03-21 10:31:33 +0900

    Release v1.2.0 from v1.10  (#41)
    * PRのテンプレートにIssueのリンクの欄を追加
    
    * Vercelのプレビューデプロイをdevelopブランチだけにするスクリプトを追加
    
    * 404ページを作成 (#34)
    
    * 記事一覧の並び順を降順にする (#35)
    
    * 記事一覧のカードの高さを揃える (#39)
    
    * SNSシェアボタンのクリック時にGAにイベントを送信 (#40)

---
+++ scripts/vercel-ignore-build-step.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+# developブランチにpushされた際のみpreview deployを行う
+# 参考: https://zenn.dev/catnose99/articles/b37104fc7ef214
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
--- src/components/ui/LINEShare/LINEShare.tsx
+++ src/components/ui/LINEShare/LINEShare.tsx
@@ -1,5 +1,6 @@
 import { ExternalLink } from "@/components/functionals/ExternalLink";
 import { LINEIcon } from "@/components/icons/LINE";
+import { EVENT_ACTION, EVENT_CATEGORY, sendEvent } from "@/libs/gtag";
 import { getLINEShareURL } from "@/utils/url";
 
 type Props = {
@@ -14,6 +15,13 @@ export const LINEShare = ({ path, title, className = "" }: Props) => {
     <ExternalLink
       href={shareUrl}
       title="LINEでシェア"
+      onClick={() =>
+        sendEvent({
+          action: EVENT_ACTION.CLICK,
+          category: EVENT_CATEGORY.SNS_SHARE_TWITTER,
+          label: path.slice(1),
+        })
+      }
       className={`hover:opacity-70 focus-visible:ring-blue-600 table ${className}`}
     >
       <LINEIcon size={28} />
