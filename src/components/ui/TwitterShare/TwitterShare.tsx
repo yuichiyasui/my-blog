22eeb999c5f7b628314b1062ac6b34e495d24512
yuichiyasui <yassan.b.a.w@gmail.com>
2022-03-21 10:11:39 +0900

    SNSシェアボタンのクリック時にGAにイベントを送信

--- src/components/ui/TwitterShare/TwitterShare.tsx
+++ src/components/ui/TwitterShare/TwitterShare.tsx
@@ -1,5 +1,6 @@
 import { ExternalLink } from "@/components/functionals/ExternalLink";
 import { TwitterIcon } from "@/components/icons/Twitter";
+import { EVENT_ACTION, EVENT_CATEGORY, sendEvent } from "@/libs/gtag";
 
 import { getTwitterShareURL } from "@/utils/url";
 
@@ -15,6 +16,13 @@ export const TwitterShare = ({ path, title, className = "" }: Props) => {
     <ExternalLink
       href={shareUrl}
       title="Twitterでシェア"
+      onClick={() =>
+        sendEvent({
+          action: EVENT_ACTION.CLICK,
+          category: EVENT_CATEGORY.SNS_SHARE_TWITTER,
+          label: path.slice(1),
+        })
+      }
       className={`hover:opacity-70 focus-visible:ring-blue-600 table ${className}`}
     >
       <TwitterIcon size={28} />
